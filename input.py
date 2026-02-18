f4a1679e0501a290a90209b35d45c22b16697421
Shruti Khanal <shruti100725@gmail.com>
2026-02-18 13:52:34 +0530

    add python program to take input from user

---
+++ input.py
@@ -0,0 +1,13 @@
+a= int(input("enter number 1 ."))
+b=int(input("enter number 2 ."))
+print("number 1 is : ",a)
+print("number 2 is : ",b)
+# c=a+b
+print("the sum of  ",a," and ",b," is ",a+b)
+
+
+
+
+
+
+
