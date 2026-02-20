66dc47dd0d47aab64a0b34864bb2eab1e03c3ab0
Shruti Khanal <shruti100725@gmail.com>
2026-02-20 21:57:34 +0530

    Add files via upload

---
+++ 1tuple.py
@@ -0,0 +1,14 @@
+fruits=[]
+f1=input("enter fruit 1 ")
+fruits.append(f1)
+f2=input("enter fruit 2")
+fruits.append(f2)
+f3=input("enter fruit 3 ")
+fruits.append(f3)
+f4=input("enter fruit 4 ")
+fruits.append(f4)
+f5=input("enter fruit 5 ")
+fruits.append(f5)
+print(fruits)
+
+
