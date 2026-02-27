cc52c955627e7b564e722e0b03d49cd2493c18f8
Shruti Khanal <shruti100725@gmail.com>
2026-02-27 13:20:47 +0530

    Add files via upload

---
+++ name_in_list.py
@@ -0,0 +1,7 @@
+name=["shruti","harry","mridul","sanskriti"]
+inp_name=input("enter your name ")
+if inp_name in name :
+    print(f"{inp_name} present in list ")
+else :
+   print(f"{inp_name}  not present in list ")    
+
