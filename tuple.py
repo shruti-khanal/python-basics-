1d206fcf4983ed9341531bed96abe2b9537473b4
Shruti Khanal <shruti100725@gmail.com>
2026-03-05 23:41:08 +0530

    Add files via upload

---
+++ tuple.py
@@ -0,0 +1,10 @@
+a=(1,2,3,4,5,5)
+b=(10,20,30,40,50)
+n=a.count(5)#counts the frequency of element 5
+print(n)
+ind=a.index(1)#returns index of 1
+print(ind)
+concat=a+b#concatenates two tuples
+print(concat)
+b=2 in a #checks if 2 is present in b tuple
+print(b)
